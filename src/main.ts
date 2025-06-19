7dc98a84a1fdecb27def2945beea4279a89a730c
Abner <abner@gmail.com>
2025-06-19 16:47:27 -0500

    CORS

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'https://front-chancito.vercel.app'],
     // credentials: true,
   });
 
